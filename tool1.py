e1a9695612f0fb61066a82fbcbcce1bc82767d5a
cbeddow <christopher.beddow@gmail.com>
2015-06-24 10:13:55 -0700

    Update tool1.py

--- tool1.py
+++ tool1.py
@@ -1,4 +1,7 @@
-import arcpy #initializing the script
+import arcpy #this lets us use geoprocessing functions from ArcGIS, like an initial rule
+
+print "GEOG 565"
+print "Sage Tezak, Sara Gregory, & Chris Beddow" #these print lines are just a test
 
 #next we will build off of an existing tool from ArcGIS and modify its script, using it as a template
 
