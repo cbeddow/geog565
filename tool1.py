ddc7171b84f5c21c73777199c537815f3700b964
Christopher Beddow <christopher.beddow@gmail.com>
2015-07-30 14:03:15 -0600

    Update tool1.py

--- tool1.py
+++ tool1.py
@@ -13,9 +13,24 @@ import arcpy, os, env
 try:
   #not sure if using current workspace is correct, thoughts? -Chris
   #I have been thinking that "CURRENT" was only to be used in the Python window of ArcMap??? -Sara
-  env.workspace = "CURRENT"
+  #I think that's right, so going to code it out -Chris
+  ##env.workspace = "CURRENT"
   
  #In here we will have our actual tool -Chris
+ 
+ #added function below that is a start on calculating FAA regions using the state
+ # called Flight Standards District Office (FSDO) regions
+ # sources: https://www.faa.gov/mobile/index.cfm?event=office.regions
+ # what is a pain is that some counties in Kentucky and Illinois are part of a different region than the rest of the state
+ # we can fix this by finding the zip codes for those counties and adding it to the region
+ # alternate idea is we can get an FSDO shapefile made first, and after geocoding we can use "clip" based on geometry instead of the below example
+def FSDOfinder(state):
+northwest = [CO, ID, MT, OR, UT, WA, WY]
+for state in states:
+  if state in northwest:
+    FAAregion = "NW":
+#incomplete code above, would need a cursor funciton to update rows in a blank "FSDO" field
+#I think making an FSDO (or finding one) shapefile and clipping will be more efficient after geocoding into XY point data
  
 except arcpy.ExecuteError:
     print arcpy.GetMessages(2)
